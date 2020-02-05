189c6699eea33ef323a4ca558b4270f9b4ad6d65
belcaid abderrahim <belcaidna@gmail.com>
2020-02-05 13:50:30 +0100

    js Stack and queue

---
+++ jsmusic.js
@@ -0,0 +1,64 @@
+class Node {
+  constructor(data, next = null, prev = null) {
+    this.data = data;
+    this.next = next;
+    this.prev = prev
+  }
+}
+class Stack {
+  constructor() {
+    this.head = null;
+    this.current = null;
+  }
+
+  push(){
+    let res = document.getElementById('res');
+    let data = document.getElementById('data').value;
+    if(!this.head){
+      this.head = this.current = new Node(data);
+    }else{
+      let tmp = this.head;
+      this.head = new Node(data,null,tmp);
+      tmp.next = this.head;
+    }
+//  res.innerHTML = '<img src="'+this.head.data+'" >';
+     res.innerHTML = '<audio controls><source src="'+this.head.data+'"></audio>';
+
+    }
+    up(){
+      if(this.current && this.current.next){
+      this.current = this.current.next;
+    }
+  }
+    down(){
+      if(this.current && this.current.prev){
+      this.current = this.current.prev;
+    }
+  }
+  showCurrent() {
+    if (this.current)
+    // res.innerHTML = '<img src="'+this.current.data+'" >';
+    res.innerHTML = '<audio controls><source src="'+this.current.data+'"></audio>';
+    
+    
+  }
+
+}
+let ll = new Stack();
+
+function push(){
+  ll.push();
+  ll.showCurrent()
+}
+
+function up(){
+  ll.up();
+  ll.showCurrent()
+
+}
+
+function down(){
+  ll.down();
+  ll.showCurrent()
+
+}
